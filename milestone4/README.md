9e5977e3f588c654ff613bcd49317af6a04a34af
Iyanu Lamina <97778885+ilamina@users.noreply.github.com>
2025-01-17 22:21:15 -0600

    Add files via upload

---
+++ milestone4/README.md
@@ -0,0 +1,5 @@
+**Milestone 4: Testing and Debugging**
+
+
+This stage is the final stage in this project. During this time, we get to finalize every aspect of our code by testing and debugging.
+Undergo code review with peers or professor.
---
+++ milestone4/reports.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-------- CSS --------->
+    <link rel="stylesheet" href="assets/css/style1.css"> 
+    
+     <!-------- JSON --------->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> <!-- Include Chart.js library -->
+   
+    <!---- ADD FONT AWESOME LINK HERE ----->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+     <!-- ------ WEB-ICON ------ -->
+     <link rel="shortcut icon" href="images/rccg.png" type="image/x-icon">
+     
+    <!-------- TITLE --------->
+     <title>Admin page</title>
+
+</head>
+<body>
+
+        <!-------- ADMIN REPORTS --------->
+        <div class="dashboard-card">
+        <header class="admin-header">
+            <div class="logo">
+            <img src="images/rccg.png" alt="User Icon">
+            </div>
+            <div class="search">
+                <input type="text" placeholder="Search...">
+            </div>
+            <div class="user-info">
+                <img src="images/avatar.png" alt="User Icon"> |
+                <span>Welcome, Admin User</span>
+            </div>
+        </header>
+        
+        <div class="container">
+        <nav class="sidebar">
+            <div class="sidebar-card">
+                <ul>
+                    <li><a href="admin panel-dashboard.php">Dashboard</a></li>
+                    <li><a href="attendance.php">Attendance</a></li>
+                    <li><a href="members.php">Members</a></li>
+                    <li><a href="upcoming events.php">Upcoming Events</a></li>
+                    <li><a href="reports.php">Reports</a></li>
+                    <li><a href="logout.php" onclick="logout()">Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+
+     <!-------- ADMIN REPORTS -main --------->
+        <div class="container-report">
+            <div class="card-report">
+                <div class="card-header-report">
+                    <h2>Attendance Trends</h2>
+                </div>
+                <div class="card-body">
+                    <canvas id="attendanceChart"></canvas>
+                </div>
+                
+                <script>
+                const ctx = document.getElementById('attendanceChart');
+
+
+                new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri'],
+                    datasets: [{
+                        label: '# Attendance Trends',
+                        data: [0.50, 0.01, 0.05, 0.10, 0.01, 0.03],
+                        borderWidth: 2
+                    }]
+                    },
+                    options: {
+                    scales: {
+                        y: {
+                        beginAtZero: true
+                        }
+                    }
+                    }
+                });
+                </script> 
+            </div>
+        </div>
+                            
+</body>
+</html>
+
+
